8eb06cc0a76228859cb899759c4e166acefdf6de
Immanuel <108454498+immanuely@users.noreply.github.com>
2024-11-10 21:52:50 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const header = document.querySelector("header h1");
-    header.addEventListener("click", function() {
-        alert("Selamat datang di website sederhana ini!");
-    });
-});
